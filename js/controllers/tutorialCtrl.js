f56147ddfe183646f746947084606cfa2828963c
Melody Plan <melodyplan@gmail.com>
2018-02-18 22:26:30 -0800

    first commit

---
+++ js/controllers/tutorialCtrl.js
@@ -0,0 +1,15 @@
+var module = angular.module('tutorialCtrlModule', [])
+
+.controller('TutorialCtrl', ["$scope", function($scope) {
+
+  $scope.tutorialObject = {};
+  $scope.tutorialObject.title = "Main Page";
+  $scope.tutorialObject.subTitle = "Sub title";
+
+  $scope.tutorialObject.bindOutput = 2;
+
+  $scope.timesTwo = function() {
+    $scope.tutorialObject.bindOutput *= 2;
+  }
+
+}]);
