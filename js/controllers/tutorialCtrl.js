652de071822cc56d66bc7bc431cd6b0a11da8929
Melody Plan <melodyplan@gmail.com>
2018-02-20 07:29:51 -0800

    learned two way data binding

--- js/controllers/tutorialCtrl.js
+++ js/controllers/tutorialCtrl.js
@@ -6,6 +6,9 @@ var module = angular.module('tutorialCtrlModule', [])
   $scope.tutorialObject.title = "Main Page";
   $scope.tutorialObject.subTitle = "Sub title";
 
+  $scope.tutorialObject.firstname = "Melody";
+  $scope.tutorialObject.lastname = "Plan";
+
   $scope.tutorialObject.bindOutput = 2;
 
   $scope.timesTwo = function() {
