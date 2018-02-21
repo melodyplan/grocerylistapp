23f42b550e9828d4804ebd896a49c224bffb8178
Melody Plan <melodyplan@gmail.com>
2018-02-20 20:23:06 -0800

    learning routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var app = angular.module('tutorialApp', ['tutorialCtrlModule']);
+var app = angular.module('tutorialApp', ['ngRoute', 'tutorialCtrlModule']);
+
+app.config(function($routeProvider) {
+  $routeProvider.when('/', [(templateUrl: 'views/tutorial.html')]);
+});
--- js/controllers/tutorialCtrl.js
+++ js/controllers/tutorialCtrl.js
@@ -1,18 +1,46 @@
-var module = angular.module('tutorialCtrlModule', [])
+var module = angular
+  .module('tutorialCtrlModule', [])
 
-.controller('TutorialCtrl', ["$scope", function($scope) {
+  .controller('TutorialCtrl', [
+    '$scope',
+    function($scope) {
+      $scope.tutorialObject = {};
+      $scope.tutorialObject.title = 'Main Page';
+      $scope.tutorialObject.subTitle = 'Sub title';
 
-  $scope.tutorialObject = {};
-  $scope.tutorialObject.title = "Main Page";
-  $scope.tutorialObject.subTitle = "Sub title";
+      $scope.tutorialObject.bindOutput = 2;
 
-  $scope.tutorialObject.firstname = "Melody";
-  $scope.tutorialObject.lastname = "Plan";
+      $scope.tutorialObject.firstname = 'Melody';
+      $scope.tutorialObject.lastname = 'P';
 
-  $scope.tutorialObject.bindOutput = 2;
+      $scope.timesTwo = function() {
+        $scope.tutorialObject.bindOutput = Calculations.timesTwo(
+          $scope.tutorialObject.bindOutput
+        );
+      };
 
-  $scope.timesTwo = function() {
-    $scope.tutorialObject.bindOutput *= 2;
-  }
+      // Calculations.pythagoreanTheorum();
+    }
+  ])
 
-}]);
+  .controller('TutorialCtrl', [
+    '$scope',
+    function($scope) {
+      $scope.secondTutorial = 'This is the second tutorial!';
+    }
+  ]);
+
+/*  .factory('Calculations', function() {
+    var calculations = {};
+
+    calculations.timesTwo = function(a) {
+      return a * 2;
+    };
+
+    calculations.pythagoreanTheorum = function(a, b) {
+      return a * a + b * b;
+    };
+
+    return calculations;
+  });
+*/
